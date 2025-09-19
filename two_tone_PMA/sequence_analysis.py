cc4649240d2a2ba14565deb426847eecff0a9163
nathanyalross <nathanyalross@gmail.com>
2025-09-19 09:26:36 -0400

    Update Documentation for new function and script

--- two_tone_PMA/sequence_analysis.py
+++ two_tone_PMA/sequence_analysis.py
@@ -1,9 +1,4 @@
-#Script that identifies sequences of activity from, in this case, a fed in ground truth, to then identify sequences and calculate
-#Sole purpose of protocol is to define order of presentations
-
-#Now: Write from scratch, goal is to take sequences and average values for the second pres in the sequence
-#Export is CSV where mouse protocol names are on the columns, transition type on the rows, and values are the averaged data
-#Input is behavior files? I suppose that would work
+#Analysis to identify sequences of presentations and investigate behavior in each sequence
 
 # Import modules to allow upstream integration of beh_functions file
 import sys
